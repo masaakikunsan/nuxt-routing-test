667db17549f2a2d1d59216d0acf537f5b4bac191
石岡将明 <ishioka0528@icloud.com>
2018-05-01 19:58:31 +0900

    initial commit

---
+++ pages/news.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <h1>News ページ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/user/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="">
+    <h1>Userページ</h1>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
